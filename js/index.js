ea8d1e158f3a570a9ad8de798e176e980173e0fc
Tim McKenna @ YCP <156977907+TimMcKennaYCP@users.noreply.github.com>
2024-03-19 16:47:22 -0400

    Tim's Upload
    I'll tell ya in class, lots of clean up

--- js/index.js
+++ js/index.js
@@ -1,14 +1,1 @@
 // Javascript
-
-$(document).ready(function(){
-      $('.your-class').slick({
-        setting: setting-value
-      });
-    });
-
-    $('.autoplay').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-      });
